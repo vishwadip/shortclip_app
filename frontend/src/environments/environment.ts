f22e460e86e5c69ebb841fd2f8defcb4c729296d
Vishwa <vishwadipsingale99@gmail.com>
2025-12-11 20:11:07 +0000

    change envirement.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,9 @@
+// export const environment = {
+//   production: false,
+//   apiBaseUrl: 'http://localhost:8080/api',   // 👈 Spring Boot base
+// };
+
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8080/api',   // 👈 Spring Boot base
+  production: true,
+  apiBaseUrl: 'http://4.250.218.36:8080/api'
 };
